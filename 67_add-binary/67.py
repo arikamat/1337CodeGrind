b5cc9e33641b03b4f7855db611d8572f5605d056
Ari Kamat <arijkamat@gmail.com>
2023-06-16 21:52:18 -0400

    finish 67

---
+++ 67_add-binary/67.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=67 lang=python3
+#
+# [67] Add Binary
+#
+from typing import *
+
+
+# @lc code=start
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        maxx = max(len(a), len(b))
+        a = str(a).rjust(maxx, "0")
+        b = str(b).rjust(maxx, "0")
+        carry = 0
+        ans = ""
+        for i in range(maxx - 1, -1, -1):
+            aDigit = int(a[i])
+            bDigit = int(b[i])
+            dig = carry + aDigit + bDigit
+            carry = dig // 2
+            dig = dig % 2
+            ans = str(dig)+ans
+        ans = str(carry)+ans
+        return str(int(ans))
+
+
+# @lc code=end
