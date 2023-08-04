70e1191271b347b74ed2ae61904b5c80b3bd90db
Ari Kamat <arijkamat@gmail.com>
2023-08-04 17:17:08 -0400

    Finish 412

---
+++ 412_fizz-buzz/412.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=412 lang=python3
+#
+# [412] Fizz Buzz
+#
+from typing import *
+# @lc code=start
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans=[]
+        for i in range(1, n+1):
+            if i%3==0 and i%5==0:
+                ans.append("FizzBuzz")
+            elif i%3==0:
+                ans.append("Fizz")
+            elif i%5==0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+        return ans
+        
+# @lc code=end
+
