6f00e64d3f04b823380520e90f8341b8f96d8cdd
Ari Kamat <arijkamat@gmail.com>
2024-07-13 13:32:38 -0400

    Optimize 274

--- 274_h-index/274.py
+++ 274_h-index/274.py
@@ -7,10 +7,26 @@ from typing import *
 # @lc code=start
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True)
-        for c, num in enumerate(citations):
-            if c >= num:
-                return c
-        return len(citations) 
+        # citations.sort(reverse=True)
+        # for c, num in enumerate(citations):
+        #     if c >= num:
+        #         return c
+        # return len(citations) 
+
+        n = len(citations)
+        arr = [0]*(n+1)
+        for i in citations:
+            if i>n:
+                arr[n]+=1
+            else:
+                arr[i]+=1
+        ct=0
+        for i in range(len(citations),-1,-1):
+            ct+=arr[i]
+            if ct>=i:
+                return i
+        return 0
+
+
 
 # @lc code=end
