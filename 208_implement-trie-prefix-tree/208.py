4814087f3311bcfac1e90d74536a3685d993c793
Ari Kamat <arijkamat@gmail.com>
2024-07-17 23:01:55 -0400

    Redo 208

--- 208_implement-trie-prefix-tree/208.py
+++ 208_implement-trie-prefix-tree/208.py
@@ -6,48 +6,39 @@
 
 # @lc code=start
 class Node:
-    def __init__(self, val="", left = None, right = None):
+    def __init__(self, val):
         self.val = val
-        self.left = left
-        self.right = right
+        self.children={}
 
 class Trie:
-    
-    def __init__(self):
-        self.head = Node()
-        self.s=set()
-    def _insert(self,root, word):
-        if root is None:
-            return Node(word)
-        if root.val<word:
-            root.right = self._insert(root.right, word)
-        elif root.val > word:
-            root.left = self._insert(root.left, word)
-        return root
-    def insert(self, word: str) -> None:
-        self.s.add(word)
-        curr = self.head
-        self._insert(curr,word)
-    def _search(self, root, word, partial):
-        if root is None:
-            return False
-        if partial and len(word) <= len(root.val) and word == root.val[0:len(word)]:
-            return True
-        elif (not partial) and word == root.val:
-            return True
-        elif word < root.val:
-            return self._search(root.left,word,partial)
-        elif word > root.val:
-            return self._search(root.right,word,partial)
-        return False
-    def search(self, word: str) -> bool:
-        curr = self.head
-        return word in self.s
-        # return self._search(curr, word, False)
 
+    def __init__(self):
+        self.head = Node("*")
+
+
+    def insert(self, word: str) -> None:
+        current = self.head
+        for i in word:
+            if i not in current.children:
+                current.children[i] = Node(i)
+            current = current.children[i]
+        current.children['*'] = Node("*")
+    def search(self, word: str) -> bool:
+        current = self.head
+        for i in word:
+            if i not in current.children:
+                return False
+            else:
+                current = current.children[i]
+        return '*' in current.children
     def startsWith(self, prefix: str) -> bool:
-        curr = self.head
-        return self._search(curr, prefix, True)
+        current = self.head
+        for i in prefix:
+            if i not in current.children:
+                return False
+            else:
+                current = current.children[i]
+        return True
         
 
 
