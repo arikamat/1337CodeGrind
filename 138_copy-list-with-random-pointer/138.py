3690a8906a53e117a0b4db3755c9fbc966abc832
Ari Kamat <arijkamat@gmail.com>
2024-08-22 16:48:14 -0400

    138 again

--- 138_copy-list-with-random-pointer/138.py
+++ 138_copy-list-with-random-pointer/138.py
@@ -16,24 +16,41 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
-        if head is None:
-            return None
-        h = head
+        res = Node(0)
+        newCurr = res
         d={}
-        while head is not None:
-            d[head] = (Node(head.val), head.next, head.random)
-            head = head.next
-        for i in d:
-            node = d[i][0]
-            if d[i][1] is not None:
-                next = d[d[i][1]][0]
-                node.next = next
-            if d[i][2] is not None:
-                rand = d[d[i][2]][0]
-                node.random = rand
+        curr = head
+        while curr:
+            t = Node(curr.val)
+            d[curr] = t
+            newCurr.next = t
+            newCurr = t
+            curr = curr.next
+        
+        curr = head
+        while curr:
+            if curr.random:
+                d[curr].random = d[curr.random]
+            curr = curr.next
+        return res.next
+        # if head is None:
+        #     return None
+        # h = head
+        # d={}
+        # while head is not None:
+        #     d[head] = (Node(head.val), head.next, head.random)
+        #     head = head.next
+        # for i in d:
+        #     node = d[i][0]
+        #     if d[i][1] is not None:
+        #         next = d[d[i][1]][0]
+        #         node.next = next
+        #     if d[i][2] is not None:
+        #         rand = d[d[i][2]][0]
+        #         node.random = rand
             
             
 
-        return d[h][0]
+        # return d[h][0]
 # @lc code=end
 
